d2e9060458bd4b26664bc5d37270cbfc7f572f1b
Maxim Moseychuk <franchesko.salias.hudro.pedros@gmail.com>
2017-01-31 00:17:43 +0300

    Initial commit

---
+++ NX_Plugin/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NXOpen;
+using NXOpen.UF;
+
+namespace NX_Plugin
+{
+    public class Program
+    {
+        // class members
+        private static Session theSession;
+        private static UI theUI;
+        private static UFSession theUfSession;
+        public static Program theProgram;
+        public static bool isDisposeCalled;
+
+        //------------------------------------------------------------------------------
+        // Constructor
+        //------------------------------------------------------------------------------
+        public Program()
+        {
+            try
+            {
+                theSession = Session.GetSession();
+                theUI = UI.GetUI();
+                theUfSession = UFSession.GetUFSession();
+                isDisposeCalled = false;
+            }
+            catch (NXOpen.NXException ex)
+            {
+                // ---- Enter your exception handling code here -----
+                // UI.GetUI().NXMessageBox.Show("Message", NXMessageBox.DialogType.Error, ex.Message);
+            }
+        }
+
+        //------------------------------------------------------------------------------
+        //  Explicit Activation
+        //      This entry point is used to activate the application explicitly
+        //------------------------------------------------------------------------------
+        public static int Main(string[] args)
+        {
+            // System.Diagnostics.Debugger.Launch();
+
+            int retValue = 0;
+            try
+            {
+                theProgram = new Program();
+                UI.GetUI().NXMessageBox.Show("Message", NXMessageBox.DialogType.Error, "OK");
+
+                //TODO: Add your application code here 
+
+                theProgram.Dispose();
+            }
+            catch (NXOpen.NXException ex)
+            {
+                // ---- Enter your exception handling code here -----
+                UI.GetUI().NXMessageBox.Show("Message", NXMessageBox.DialogType.Error, "Error");
+
+            }
+            return retValue;
+        }
+
+
+        //------------------------------------------------------------------------------
+        // Following method disposes all the class members
+        //------------------------------------------------------------------------------
+        public void Dispose()
+        {
+            try
+            {
+                if (isDisposeCalled == false)
+                {
+                    //TODO: Add your application code here 
+                }
+                isDisposeCalled = true;
+            }
+            catch (NXOpen.NXException ex)
+            {
+                // ---- Enter your exception handling code here -----
+
+            }
+        }
+    }
+}
