69f6279ec65014cc2443c79c10a4f5d6bc76ceb1
Maxim Moseychuk <franchesko.salias.hudro.pedros@gmail.com>
2017-01-31 05:42:33 +0300

    Refactoring

--- NX_Plugin/Program.cs
+++ NX_Plugin/Program.cs
@@ -46,100 +46,86 @@ namespace NX_Plugin
             {
                 theProgram = new Program();
 
-                Tag UFPart1;
-                string name1 = "model_k";
-                int units1 = 1;
-                theUfSession.Part.New(name1, units1, out UFPart1);
-
                 const double l = 50;
                 const double r = 5;
+                const double offset = 1;
+                const double angle = 45;
+                const double rp = r / 6;
+                const double h = 3;
+
+                Tag UFPart1;
+                theUfSession.Part.New("model_ka", 1, out UFPart1);
 
                 Tag cylinder;
                 double[] point = { 0, 0, 0 };
                 double[] direction = { 1, 0, 0 };
-                theUfSession.Modl.CreateCylinder(FeatureSigns.Nullsign, UFPart1, point, l.ToString(), (r*2).ToString(), direction, out cylinder);
+                theUfSession.Modl.CreateCylinder(FeatureSigns.Nullsign, UFPart1, point, l.ToString(), (r * 2).ToString(), direction, out cylinder);
 
 
                 Tag cyl_tag, obj_id_camf;
                 Tag[] Edge_array_cyl;
                 int ecount;
-
                 theUfSession.Modl.AskFeatBody(cylinder, out cyl_tag);
                 theUfSession.Modl.AskBodyEdges(cyl_tag, out Edge_array_cyl);
                 theUfSession.Modl.AskListCount(Edge_array_cyl, out ecount);
 
                 ArrayList arr_list = new ArrayList();
 
-                for (int ii = 0; ii < ecount; ii++)
+                for (int i = 0; i < ecount; ++i)
                 {
                     Tag edge;
-
-                    theUfSession.Modl.AskListItem(Edge_array_cyl, ii, out edge);
+                    theUfSession.Modl.AskListItem(Edge_array_cyl, i, out edge);
                     arr_list.Add(edge);
                 }
-                
-                Tag[] list = (Tag[])arr_list.ToArray(typeof(Tag));
-                string offset1 = "1";
-                string offset2 = "1";
-                string ang = "45";
-                theUfSession.Modl.CreateChamfer(3, offset1, offset2, ang, list, out obj_id_camf);
 
+                Tag[] list = (Tag[])arr_list.ToArray(typeof(Tag));
+                theUfSession.Modl.CreateChamfer(3, offset.ToString(), offset.ToString(), angle.ToString(), list, out obj_id_camf);
 
                 ////////////////////////////////
+                
+                double[] l1_endpt1 = { 0, -r, -rp };
+                double[] l1_endpt2 = { 0, r, -rp };
+                double[] l2_endpt1 = l1_endpt2;
+                double[] l2_endpt2 = { 0, r, rp };
+                double[] l3_endpt1 = l2_endpt2;
+                double[] l3_endpt2 = { 0, -r, rp };
+                double[] l4_endpt1 = l3_endpt2;
+                double[] l4_endpt2 = l1_endpt1;
 
-
-                {
-                    const double rp = r / 6;
-
-                    double[] ll1_endpt1 = { 0, -r, -rp };
-                    double[] ll1_endpt2 = { 0, r, -rp };
-                    double[] ll2_endpt1 = ll1_endpt2;
-                    double[] ll2_endpt2 = { 0, r, rp };
-                    double[] ll3_endpt1 = ll2_endpt2;
-                    double[] ll3_endpt2 = { 0, -r, rp };
-                    double[] ll4_endpt1 = ll3_endpt2;
-                    double[] ll4_endpt2 = ll1_endpt1;
-
-                    UFCurve.Line[] llines = {
-                        new UFCurve.Line() { start_point = ll1_endpt1, end_point = ll1_endpt2 },
-                        new UFCurve.Line() { start_point = ll2_endpt1, end_point = ll2_endpt2 },
-                        new UFCurve.Line() { start_point = ll3_endpt1, end_point = ll3_endpt2 },
-                        new UFCurve.Line() { start_point = ll4_endpt1, end_point = ll4_endpt2 }
+                UFCurve.Line[] lines = {
+                        new UFCurve.Line() { start_point = l1_endpt1, end_point = l1_endpt2 },
+                        new UFCurve.Line() { start_point = l2_endpt1, end_point = l2_endpt2 },
+                        new UFCurve.Line() { start_point = l3_endpt1, end_point = l3_endpt2 },
+                        new UFCurve.Line() { start_point = l4_endpt1, end_point = l4_endpt2 }
                     };
 
-                    Tag[] llines_tags = new Tag[llines.Length];
-                    for (int i = 0; i < llines.Length; ++i)
-                        theUfSession.Curve.CreateLine(ref llines[i], out llines_tags[i]);
+                Tag[] lines_tags = new Tag[lines.Length];
+                for (int i = 0; i < lines.Length; ++i)
+                    theUfSession.Curve.CreateLine(ref lines[i], out lines_tags[i]);
+                
+                string[] limit4 = { "0", h.ToString() };
+                double[] dummy = new double[3];
+                double[] direction4 = { 1.0, 0.0, 0.0 };
+                Tag[] extruded;
 
-                    string taper_angle4 = "0.0";
-                    double h = 3;
-                    string[] limit4 = { "0", h.ToString() };
-                    double[] ref_pt4 = new double[3];
-                    double[] direction4 = { 1.0, 0.0, 0.0 };
-                    Tag[] features4;
-
-                    theUfSession.Modl.CreateExtruded(
-                                    llines_tags,
-                                    taper_angle4,
-                                    limit4,
-                                    ref_pt4,
-                                    direction4, FeatureSigns.Negative,
-                                    out features4);
-
-
-                    
-
-                } 
+                theUfSession.Modl.CreateExtruded(
+                                lines_tags,
+                                "0",
+                                limit4,
+                                dummy,
+                                direction4,
+                                FeatureSigns.Negative,
+                                out extruded);
 
                 theProgram.Dispose();
 
 
+
             }
-            catch (NXOpen.NXException)
+            catch (NXOpen.NXException ex)
             {
                 // ---- Enter your exception handling code here -----
-                UI.GetUI().NXMessageBox.Show("Message", NXMessageBox.DialogType.Error, "Error");
-
+                UI.GetUI().NXMessageBox.Show("Message", NXMessageBox.DialogType.Error, ex.Message);
             }
             return retValue;
         }
